b5a1c6a94eb50b5e331f007aaf6f25d88656cfda
Maxim Sorkin <msork.uni2004@gmail.com>
2025-06-17 23:30:36 -0400

    Update update-curseforge-solus.sh

--- scripts/update-curseforge-solus.sh
+++ scripts/update-curseforge-solus.sh
@@ -12,9 +12,9 @@ VERSION=$(sed -n 2p control | grep -Po '(?<=Version: )\d+\.\d+\.\d+')
 rm debian-binary data.tar.xz control control.tar.gz curseforge-latest-linux.deb
 
 # COPY SCRIPT AND FILES
-wget 'https://raw.githubusercontent.com/msork/curseforge-solus/refs/heads/master/scripts/ep-update.py' -O ep-update.py
-wget 'https://raw.githubusercontent.com/msork/curseforge-solus/refs/heads/master/games/curseforge/actions.py' -O actions.py
-wget 'https://raw.githubusercontent.com/msork/curseforge-solus/refs/heads/master/games/curseforge/pspec.xml' -O pspec.xml
+wget 'https://raw.githubusercontent.com/msork/my-3rdparty-solus/refs/heads/master/scripts/ep-update.py' -O ep-update.py
+wget 'https://raw.githubusercontent.com/msork/my-3rdparty-solus/refs/heads/master/games/curseforge/actions.py' -O actions.py
+wget 'https://raw.githubusercontent.com/msork/my-3rdparty-solus/refs/heads/master/games/curseforge/pspec.xml' -O pspec.xml
 
 # RUN SCRIPT
 chmod +x ep-update.py
