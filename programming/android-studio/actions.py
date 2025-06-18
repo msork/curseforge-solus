3ad6991f4676cbefa5466883f0aedc2d813b3f8f
Maxim Sorkin <msork.uni2004@gmail.com>
2025-06-17 22:52:22 -0400

    Delete programming directory

--- programming/android-studio/actions.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-from pisi.actionsapi import get, pisitools
-
-def install():
-    pisitools.insinto("/opt/android-studio", "*")
-    pisitools.dosym("/opt/android-studio/bin/studio.sh", "/usr/bin/android-studio")
